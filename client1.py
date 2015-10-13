3082b298992fc9c9b1a1a6edbb44b01f07ee7d4c
Geoff Dabu <geoffdabu@geoffs-MacBook-Pro.local>
2015-10-13 00:53:58 -0700

    client payload

--- client1.py
+++ client1.py
@@ -4,14 +4,13 @@ import sys, os, argparse, socket, logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
-
-
+def remoteExecute(pkt):
+	print pkt
 
 def main():
-	pkt = IP(dst="192.168.0.9")/UDP()
+	pkt = IP(dst="192.168.0.9")/UDP(dport=1000, sport=2000)/'zzzz'
 	send(pkt)
-
+	sniff(filter="ip and host 192.168.0.9", prn=remoteExecute)
 
 if __name__ == '__main__':
 	main()
-
