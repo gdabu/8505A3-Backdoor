d657a75d333afb711535e1270a95cb2294c41a41
Geoff Dabu <geoffdabu@geoffs-MacBook-Pro.local>
2015-10-12 23:03:47 -0700

    setup

--- client1.py
+++ client1.py
@@ -4,20 +4,14 @@ import sys, os, argparse, socket, logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
-def knock(host, ports):
-	host = args.destIp
-	ports = []
-	for port in args.ports:
-		ports.append(port)
-	for i in ports:
-		pkt = IP(dst=host)/TCP(sport=int(i), dport=RandNum(0,65355), window=4096)
-		send(pkt)
+
+
 
 def main():
-	knock(args.destIp, args.ports)
+	pkt = IP(dst="192.168.0.9")/UDP()
+	send(pkt)
+
 
 if __name__ == '__main__':
-	try:
-		main()
-	except KeyboardInterrupt:
-		print 'Exiting..'
+	main()
+
--- server1.py
+++ server1.py
@@ -8,25 +8,10 @@ knock = [2000,2001,2002]
 count = 0
 
 def remoteExecute(pkt):
-    global count
-    dstIP = pkt[1].dst
-    wnd = pkt[2].window
-    if wnd == 4096:
-        port = pkt[2].sport
-        if port == knock[0]:
-            count +=1
-        if port == knock[1]:
-            count +=1
-        if port == knock[2]:
-            count +=1
-        if count == 3:
-            print 'Authenticated!..Establishing Connection..'
-            createServer(dstIP, 8505)
-    elif count < 3:
-        return
+    
 
 def main():
-    sniff(count= 5, filter="ip and tcp", prn=remoteExecute)
+    sniff(filter="ip and tcp", prn=remoteExecute)
 
 if __name__ == '__main__':
     try:
